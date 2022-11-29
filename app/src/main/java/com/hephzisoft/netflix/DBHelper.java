3114eecc58ad48cfc8ff7dd22374ab9fc81be352
hephzisoft <hephzisoft@gmail.com>
2022-11-29 11:44:43 +0100

    made some certain changes

--- app/src/main/java/com/hephzisoft/netflix/DBHelper.java
+++ app/src/main/java/com/hephzisoft/netflix/DBHelper.java
@@ -4,15 +4,19 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.view.View;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class DBHelper extends SQLiteOpenHelper {
-    private final static String db_name = "netflix.db";
+
+    private final static String db_name = "foodie.db";
     private final static int version = 1;
+
 
     Context context;
 
@@ -23,47 +27,54 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table users(id integer primary key autoincrement,  text unique, firstname text, lastname text, email text, password text,phone_number integer, is_loggedin boolean )");
+        db.execSQL("create table users (id integer  primary key autoincrement , username text unique , email text , password text, is_loggedin boolean)");
+        db.execSQL("create table dishes (id integer primary key autoincrement, name text, ingredients text, process text, image_url text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists users");
+        db.execSQL("drop table if exists dishes");
         onCreate(db);
     }
-//      CREATE USER
-    public void createUser(View view, String username, String first_name, String last_name, String email, String password, int phoneNumber) {
+
+//    CREATE USER
+
+    public void createUser(View view, String username, String email, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("usernmae", username);
-        contentValues.put("first_name", first_name);
-        contentValues.put("last_name", last_name);
+        contentValues.put("username", username);
         contentValues.put("email", email);
         contentValues.put("password", password);
-        contentValues.put("phoneNumber", phoneNumber);
+        contentValues.put("is_loggedin", true);
 
         long result = db.insert("users", null, contentValues);
+
+
     }
-//    VERIFY USER
-    public boolean verifyUser(String username,String password){
+
+    //        VERIFY USER\
+    public boolean verifyUser(String username, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("select id from users where username = ? and passeord = ?",new String[]{username, password});
+        Cursor cursor = db.rawQuery("select id from users where username  = ? and password = ?", new String[]{username, password});
 
 
-        if (cursor.getCount() >0){
+        if (cursor.getCount() > 0) {
             ContentValues contentValues = new ContentValues();
             contentValues.put("is_loggedin", true);
 
-            long result = db.update("users", contentValues,"username = ?, passwordd =?", new String[]{username, password});
+            long result = db.update("users", contentValues, "username = ? and password = ?", new String[]{username, password});
 
-            if(result == -1){
-                Toast.makeText(context, "An error occurred", Toast.LENGTH_SHORT).show();
-            }
+            if (result == -1)
+                Toast.makeText(context, "An error occurred while fetching data", Toast.LENGTH_SHORT).show();
+
             return true;
-        }else{
+        } else {
             return false;
         }
+
     }
+
     public String[] getLoggedInUser() {
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery("select username from users where is_loggedin = 1", new String[]{});
@@ -77,4 +88,41 @@ public class DBHelper extends SQLiteOpenHelper {
 
         return new String[]{String.valueOf(count), loggedin_user};
     }
+//    CREATE DISH
+
+    public void createDish(String name, String ingredients, String process, String image_url) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put("name", name);
+        cv.put("ingredients", ingredients);
+        cv.put("process", process);
+        cv.put("image_url", image_url);
+
+        db.insert("dishes", null, cv);
+    }
+    public Cursor getDishes(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("select id, name , image_url, ingredients , process from dishes", new String[]{});
+    }
+
+//    DELETE DISHES
+
+    public boolean deleteDish(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        try{
+        db.delete("dishes",String.format("id= %d", id), new String[]{});
+
+        return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void editDish(int id, String ingredients, String process, String image_url){
+
+    }
 }
+
+
